2ca0e9bcbaee19bd6e3189773d9483f4bfdbf412
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-09-15 01:47:10 +0900

    use aeson lens

--- Settings.hs
+++ Settings.hs
@@ -5,13 +5,37 @@ module Settings
   , chan
   , nick
   , password ) where
-server :: String
-server   = "irc.freenode.org"
-port :: Int
-port     = 6667
-chan :: String
-chan     = "#tutbot-testing"
-nick :: String
-nick     = "hsbot"
-password :: String
-password = "password"
+import Data.Aeson (decode, Value)
+import Data.Maybe
+import qualified Data.ByteString.Lazy as B
+import Data.Text
+import Control.Lens
+import Data.Aeson.Lens
+import Prelude
+
+server :: IO String
+server   = readSetting "server"
+port :: IO Int
+port     = readSetting' "port"
+chan :: IO String
+chan     = readSetting "channel"
+nick :: IO String
+nick     = readSetting "nick"
+password :: IO String
+password = readSetting "password"
+
+readSetting :: String -> IO String
+readSetting val = do
+  fstr <- B.readFile "setting.json"
+  let v = decode fstr :: Maybe Value
+  let retval = v ^. key (pack val) :: Maybe String
+  let _retval = fromJust retval
+  return _retval
+
+readSetting' :: String -> IO Int
+readSetting' val = do
+  fstr <- B.readFile "setting.json"
+  let v = decode fstr :: Maybe Value
+  let retval = v ^. key (pack val) :: Maybe Int
+  let _retval = fromJust retval
+  return _retval
