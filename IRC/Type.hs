3ab9686c759c3f2e89d607956620e48f997f3bc4
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-09-18 01:07:11 +0900

    extend data Bot to treat ssl connection

--- IRC/Type.hs
+++ IRC/Type.hs
@@ -1,7 +1,10 @@
 module IRC.Type where
 import Control.Monad.Reader
 import System.IO
+import Network.TLS
+import Data.Maybe
 
 -- | The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
 type Net = ReaderT Bot IO
-data Bot = Bot { socket :: Handle }
+data Bot = Bot { socket :: Handle
+               , tlsCtx :: Maybe TLSCtx }
