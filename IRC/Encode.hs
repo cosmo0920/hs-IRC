96cff81dd9e006bfb0b704edd5996e30c326ebe4
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-10-14 20:07:31 +0900

    create IRC.Encode module

---
+++ IRC/Encode.hs
@@ -0,0 +1,27 @@
+module Encode where
+
+import Data.Text.Encoding
+import qualified Data.Text                     as T
+import qualified Data.ByteString               as P (ByteString) -- type name only
+import qualified Data.ByteString               as S
+import qualified Data.ByteString.Internal      as S
+import qualified Data.ByteString.Unsafe        as S
+import qualified Data.ByteString.Char8         as B
+import qualified Data.ByteString.Internal      as BI
+import qualified Data.ByteString.Lazy.Char8    as BL
+import qualified Data.ByteString.Lazy.Internal as BLI
+
+-- TODO: botで使っている型に合わせる
+mkByteStringWithEncoding :: String -> B.ByteString
+mkByteStringWithEncoding = encodeUtf8 . T.pack
+
+-- |/O(1)/ Convert a strict 'ByteString' into a lazy 'ByteString'.
+-- | sadly hack...
+-- | Bacause of bytestring 0.9.x version, nothing provides Lazy <-> Strict conversion function.
+fromStrict' :: P.ByteString -> BL.ByteString
+fromStrict' bs | S.null bs = BLI.Empty
+               | otherwise = BLI.Chunk bs BLI.Empty
+
+byteStringToString :: B.ByteString -> String
+byteStringToString str = do
+  B.unpack str
