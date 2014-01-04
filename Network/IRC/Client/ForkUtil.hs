4e72a2406c9d21becc9ecf239e73aab1a77dda0a
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-01-04 21:15:53 +0900

    fix build error for ghc 7.6 or higher

--- Network/IRC/Client/ForkUtil.hs
+++ Network/IRC/Client/ForkUtil.hs
@@ -1,7 +1,10 @@
 module Network.IRC.Client.ForkUtil
-  ( Network.IRC.Client.ForkUtil.forkFinally
-  -- re-export
-  , module Control.Concurrent ) where
+  ( -- re-export,
+    module Control.Concurrent
+#if __GLASGOW_HASKELL__ <= 704
+  , Network.IRC.Client.ForkUtil.forkFinally
+#endif
+  ) where
 import Prelude
 
 import Control.Exception.Base as Exception
@@ -9,7 +12,7 @@ import Control.Concurrent
 
 -- | base 4.5.*'s Control.Concurrent package does not export forkFinally function.
 --
---   So, implement forkFinally.
+-- | So, implement forkFinally.
 forkFinally :: IO a -> (Either SomeException a -> IO ()) -> IO ThreadId
 forkFinally action and_then =
   mask $ \restore ->
